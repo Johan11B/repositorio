f851bcbd7f44e448bb7c93868801d6ed0ae50d85
Johan11B <jbriseno@ucundinamarca.edu.co>
2025-02-28 18:22:13 -0500

    Operadores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -77,6 +77,111 @@ public class Main {
         }else{
             System.out.println("El número "+numero+" es impar");
         }
+
+        //---Operadores---//
+
+        //Variables
+        int a=5;
+        int b=6;
+
+        //---Aritméticos--//
+        System.out.println("--ARITMETICOS---");
+        int suma = a + b; // Suma
+        int resta = a - b; // Resta
+        int multiplicacion = a * b; // Multiplicación
+        double division = 30.0/ 10.0; // División se utiliza double
+        int modulo = b % a ; // Resto de la división
+        System.out.println("Suma= "+suma);
+        System.out.println("Resta="+resta);
+        System.out.println("Multiplicacion= "+multiplicacion);
+        System.out.println("Division= "+division);
+        System.out.println("Modulo= "+modulo);
+
+        //---LOGICOS---//
+        System.out.println("----LOGICOS---");
+        boolean and = (b > a) && (a < b); // AND (ambas condiciones deben ser verdaderas)
+        boolean or = (b > a) || (a > b); // OR (al menos una condición debe ser verdadera)
+        boolean not = !(b > a); // NOT (invierte la condición)
+
+        System.out.println("AND= "+and);
+        System.out.println("OR= " +or);
+        System.out.println("NOT= "+not);
+
+        //---RELACIONALES (COMPARACION)---//
+        int a1=5;
+        int b1=4;
+        System.out.println("---RELACIONALES---");
+        System.out.println("Igualdad-->a1 == b1: " + (a1 == b1));//Igualdad
+        System.out.println("Diferente-->a1 != b1: " + (a1 != b1));//Diferente
+        System.out.println("Mayor que-->a1 > b1: " + (a1 > b1));//Mayor que
+        System.out.println("Menor que-->a1 < b1: " + (a1 < b1));//Menor que
+        System.out.println("Mayor o igual-->a1 >= b1: " + (a1 >= b1));//Mayor o igual
+        System.out.println("Menor o igual-->a1 <= b1: " + (a1 <= b1));//Menor o igual
+
+        //---ASIGNACIÓN---//
+
+        System.out.println("---ASIGNACION---");
+
+        //Asignación simple
+        int s = 7;
+        System.out.println("Asignacion simple--> s=7: " +s);
+        //Suma y asignación
+        s += 5;
+        System.out.println("Suma y asignacion--> s += 5: " + s);
+        //Resta y asignación
+        s -= 2;
+        System.out.println("Resta y asignacion-->s -= 2: " + s);
+        //Multiplicación y asignación
+        s *= 3;
+        System.out.println("Multiplicacion y asignacion--> s *= 3: " + s);
+        //División y asignación
+        s/= 2;
+        System.out.println("Division y asignacion--> s /= 2: " + s);
+        //Módulo y asignación
+        s %= 3;
+        System.out.println("Modulo y asignacion--> s %= 3: " + s);
+
+        //---INCREMENTO Y DECREMENTO---//
+
+        System.out.println("---INCREMENTO Y DECREMENTO---");
+
+        int i=2;
+
+        System.out.println("Incremento en 1--> i++: " + (i++)); //Incremento en 1
+        System.out.println("Valor despues de i++: "+i);
+        System.out.println("Incremento en 1--> ++i: " + (++i)); // Incremento en 1
+        System.out.println("Decremento en 1--> i--: " + (i--)); // Decremento en 1
+        System.out.println("Valor despues de i--:"+i);
+        System.out.println("Decremento en 1--> --i: " + (--i)); // Decremento en 1
+
+
+        //---TERNARIO---//
+
+        System.out.println("---TERNARIO---");
+
+        //variable = (condición) ? valor_si_verdadero : valor_si_falso;
+
+        int edad = 15;
+        String mensaje = (edad >= 18) ? "Mayor de edad" : "Menor de edad";
+        System.out.println(mensaje); // Imprime "Menor de edad"
+
+        int num=-2;
+        String resultado = (num >= 0) ? "Numero positivo" : "Numero negativo";
+        System.out.println(resultado); // Imprime "Número negativo"
+
+        //---BIT A BIT---//
+
+        //Manipula los bits individuales de los números enteros
+        int x = 5;  // 0101 en binario
+        int y = 3;  // 0011 en binario
+        System.out.println("---BIT A BIT---");
+        System.out.println("AND--> x & y = " + (x & y));  // 0001 -> 1
+        System.out.println("OR--> x | y = " + (x | y));  // 0111 -> 7
+        System.out.println("XOR--> x ^ y = " + (x ^ y));  // 0110 -> 6
+        System.out.println("NOT--> ~x = " + (~x));        // Complemento de 5 -> -6
+        System.out.println("Desplazamiento a la izquierda--> x << 1 = " + (x << 1)); // 1010 -> 10
+        System.out.println("Desplazamiento a la derecha--> x >> 1 = " + (x >> 1)); // 0010 -> 2
+        System.out.println("Desplazamiento a la derecha sin signo--> x >>> 1 = " + (x >>> 1)); // 0010 -> 2
         }
     }
 }
