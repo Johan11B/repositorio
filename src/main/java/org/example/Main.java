8440f6a03a8fcae0cc1e4c8c7c68d1cb65babffa
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-02-28 20:40:40 -0500

    Ultimos cambios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,17 +20,17 @@ public class Main {
         //(byte, short, int, long) enteros
 
         //Ejemplo de byte (-128--127)
-        System.out.println("Por favor ingrese su edad ´por favor: ");
+        System.out.println("\nPor favor ingrese su edad: ");
         byte edad = entrada.nextByte();
         System.out.println("Su edad es: "+edad);
 
         //Ejemplo short (-32,768--32,767)
-        System.out.println("Ingrese su estatura (en cm)");
+        System.out.println("\nIngrese su estatura (en cm)");
         short estaturacm = entrada.nextShort();
         System.out.println("La estatura indicada es de: "+estaturacm+"cm");
 
         //Ejemplo de int (-2,147,483,648--2,147,483,647)
-        System.out.println("Por favor indique su número de documento sin puntos ni comas");
+        System.out.println("\nPor favor indique su número de documento sin puntos ni comas");
         int document = entrada.nextInt();
         System.out.println("Su número de documento es: "+document);
 
@@ -42,24 +42,24 @@ public class Main {
         //distancia alpha centauri en metros
         //hay que convertir distanciaAlpha a long
         long distanciaAlphaMetros = anioLuzenMetros*(long)distanciaAlpha;
-        System.out.println("La distancia de Alpha Centauri a la Tierra es de "+distanciaAlphaMetros+" metros.");
+        System.out.println("\nLa distancia de Alpha Centauri a la Tierra es de "+distanciaAlphaMetros+" metros.");
 
 
         //Tipos de dato decimal (float, double)
 
         //Ejemplo de float (se necesita agregar "f" o "F" al número
         float velocidad = 120.5f;
-        System.out.println("La velocidad a la que iba el auto era de: "+velocidad+" km/h");
+        System.out.println("\nLa velocidad a la que iba el auto era de: "+velocidad+" km/h");
 
         //Ejemplo de double
-        System.out.println("Precio de un producto después de aplicar un descuento");
+        System.out.println("\nPrecio de un producto después de aplicar un descuento");
         int valor_producto = 210;
         double valor_final = valor_producto-(valor_producto*0.15);
         System.out.println("El precio del producto con el descuento es de: $"+valor_final);
 
         //Tipo de dato char (almacena caracteres unicode
         //Ejemplo de char
-        System.out.println("Son ciudades alemanas: ");
+        System.out.println("\nSon ciudades alemanas: ");
         System.out.println("A. Munich, Frankfurt, Amsterdam, Copenague");
         System.out.println("B. Berlín, Dortmund, Bremen, Munich");
         System.out.println("C. Zurich, Stuttgart, Berlín, Praga");
@@ -75,7 +75,7 @@ public class Main {
 
         //Tipos de dato boolean (almacena cierto o falso)
         //Ejemplo de boolean
-        System.out.println("Ingrese un  numero para verificar si es par o impar");
+        System.out.println("\nIngrese un  numero para verificar si es par o impar");
         int numero = entrada.nextInt();
         boolean esPar = (numero%2==0);
         if (esPar){
@@ -130,7 +130,7 @@ public class Main {
         //Estructura de control FOR: Ejecuta una determinada cantidad de veces.
         //ejemplo imprimir numeros del 1 al 10
 
-        System.out.println("Estructura for: ");
+        System.out.println("\nEstructura for: ");
         for(int i = 1; i <11; i++){
             System.out.println(i);
         }
@@ -139,7 +139,7 @@ public class Main {
         //El ciclo for-each en Java es una estructura de control que permite recorrer los elementos de
         // un array o una colección de manera sencilla y eficiente, sin necesidad de gestionar índices manualmente.
 
-        System.out.println("Estructura for each");
+        System.out.println("\nEstructura for each");
         int numeros[] = new int[5];
         numeros[0] = 34;
         numeros[1] = 89;
@@ -156,7 +156,7 @@ public class Main {
 
         //ESTRUCTURA DE CONTROL SWITCH
         System.out.println();
-        System.out.println("Estructura switch: ");
+        System.out.println("\nEstructura switch: ");
         System.out.println("Ingresa tu nota obtenida: 1,2,3,4 o 5");
         int nota22 = entrada.nextInt();
 
@@ -185,8 +185,6 @@ public class Main {
                 System.out.println("Ingresaste una nota invalida, intenta de nuevo");
                 break;
         }
-
-
 
 
         //---Operadores---//
